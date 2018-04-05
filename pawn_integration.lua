1e7b318b7d0e092ab28960594ea43d03a19836f7
Dawer <iDawer@users.noreply.github.com>
2018-04-05 11:37:17 +0500

    Added version number "0.1.0-alpha"

--- pawn_integration.lua
+++ pawn_integration.lua
@@ -7,6 +7,7 @@
            file using any text editor.
     Tested with Ludwig  1.6.0 and Pawn 1.1.2 for WoW 2.4.3.
     Author: Dawer
+    Version: v0.1.0-alpha
 	Commands:
 		/lw --pawn|Scale name
             Sorts by Pawn's scale values.
