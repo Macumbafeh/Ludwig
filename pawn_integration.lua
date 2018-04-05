fb9bfa75b20fcc34fb90ebfb62960be98f480b86
Dawer <iDawer@users.noreply.github.com>
2018-04-05 12:09:18 +0500

    Added "allowed to use" filtering.

--- pawn_integration.lua
+++ pawn_integration.lua
@@ -86,6 +86,27 @@ do
     end
 end
 
+--[[
+Checks the class allowed to use current item.
+The current item is set to PawnPrivateTooltip after PawnGetItemData(...) call
+even the item was in cache (cache miss bug in Pawn).
+Suppose cache miss bug is not fixed.
+Parameters:
+    className: Class name in English.
+]]
+local function isAllowedForClass(className)
+    local tooltip = PawnPrivateTooltip
+    local leftLineText
+    for i = 1, tooltip:NumLines() do
+        leftLineText = getglobal('PawnPrivateTooltipTextLeft' .. i):GetText()
+        if string.match(leftLineText, "^Classes:") then
+            return string.match(leftLineText, className) ~= nil
+        end
+    end
+    -- If not specified allow by default.
+    return true
+end
+
 function Ludwig:GetItems(name, quality, typefilters, minLevel, maxLevel)
     -- disable sorting if no filter set
     if not (name or quality or (typefilters and next(typefilters) ~= nil) or minLevel or maxLevel) then
@@ -97,11 +118,15 @@ function Ludwig:GetItems(name, quality, typefilters, minLevel, maxLevel)
     if PawnGetItemData and Ludwig.pawnSort then
         -- cache Pawn values
         local itemData, pval
+        local playerClass = UnitClass('player')
         pawnCache = {}
         underestimatedCache = {}
         for _, id in pairs(items) do
             itemData = PawnGetItemData(Ludwig:GetItemLink(id))
             pval = PawnGetSingleValueFromItem(itemData, Ludwig.pawnScaleName)
+            if not isAllowedForClass(playerClass) then
+                pval = nil
+            end
             if pval then
                 pawnCache[id] = pval
             end
